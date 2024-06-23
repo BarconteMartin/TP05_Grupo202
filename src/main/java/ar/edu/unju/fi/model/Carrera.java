0f46ac723d797d64cc6e220bf576854ba2170556
Martin Barconte <Martin Barconte@Martin>
2024-06-23 09:31:29 -0300

    Commit Avance Carrera. BarconteMartin

--- src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,5 +1,13 @@
 package ar.edu.unju.fi.model;
+import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
+@Data
+@Component
 public class Carrera {
-
+	private String codigo;
+	private String nombre;
+	private int cantidadAnios;
+	private boolean estado;
 }
