e255deaa81feee07df246b1ef07fb3d5b1f2c7ea
wiber <wiber@DESKTOP-29TD6KD>
2024-07-08 17:30:10 -0300

    PARTE FINALIZADA - DOCENTE. Lara Altamirano.

---
+++ src/main/java/ar/edu/unju/fi/DTO/MateriaDTO.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.DTO;
+
+import org.springframework.stereotype.Component;
+
+import ar.edu.unju.fi.model.Carrera;
+import ar.edu.unju.fi.model.Docente;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+public class MateriaDTO {
+	private Integer codigoMateria;
+	private String nombreMateria;
+	private String cursoMateria;
+	private int duracionMateria;
+	private boolean modalidadMateria;
+	private Docente docenteMateria;
+	private Carrera carreraMateria;
+}
---
+++ src/main/java/ar/edu/unju/fi/service/CarreraService.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.DTO.CarreraDTO;
+import ar.edu.unju.fi.model.Carrera;
+
+@Service
+public interface CarreraService {
+	public int buscarPosicionCarrera(String codigo);
+	public Carrera buscarCarrera(String codigo);
+	public void borrarCarrera(String codigo);
+	public void modificarCarrera(Carrera c);
+	public void guardarCarrera(Carrera c);
+	public List<CarreraDTO> mostrarCarrerasDTO();
+	public List<Carrera> mostrarCarreras();	
+}
